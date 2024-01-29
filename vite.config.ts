bf6d48e26f882ba6a85875f0553eba2627f75ce5
Joel Jimenez <joel.jimenez@coppel.com>
2024-01-29 10:44:15 -0700

    Fix base in vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jo3ljimenez.gitlab.io/confirmation'
+  base: 'https://jo3ljimenez.github.io/confirmation'
 })
