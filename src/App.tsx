83a67182d109643cecb9c8dd8eeee858dbbebc6e
Joel Jimenez <joel.jimenez@coppel.com>
2024-01-29 12:37:32 -0700

    ADD information section below the header, add Permission policy to fix warning in index

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 // import { useState } from 'react'
 import './components/globalStyle.css'
-import Header from './components/header/header'
+import HeaderSection from './components/header/header'
+import InformationSection from './components/information/information'
 import {  GALLERY06 } from './globalConstants'
-
 function App() {
   return (
     <>
       <ul>
-        <li><Header img={GALLERY06}/></li>
+        <li className='section'><HeaderSection img={GALLERY06}/></li>
+        <li className='section'><InformationSection 
+                                  title='Estás invitando'
+                                  message='Queremos que seas parte de este momento especial' /></li>
       </ul>
     </>
   )
