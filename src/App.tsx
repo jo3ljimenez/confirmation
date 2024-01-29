70813465ccb2cd8cfe8e491fb51ad6aa16471474
Joel Jimenez <joel.jimenez@coppel.com>
2024-01-29 12:04:57 -0700

    Remove unused images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 // import { useState } from 'react'
 import './components/globalStyle.css'
 import Header from './components/header/header'
-import {  CLOTHES, CONFIRMATION, GALLERY01, GALLERY02, 
-          GALLERY03, GALLERY04, GALLERY05, GALLERY06, GALLERY07, 
-          GALLERY08, GALLERY09, GALLERY10, GALLERY11, GIFTS, RINGS, 
-          WINE } from './globalConstants'
+import {  GALLERY06 } from './globalConstants'
 
 function App() {
   return (
